9d875ffe98a5b95be21fa21cc50ffdda36be4f18
adam <adam@boduch.net>
2020-04-13 14:52:38 +0200

    usuniecie plikow js

--- build/elasticsearch.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const elasticsearch_1 = require("@elastic/elasticsearch");
-exports.default = new elasticsearch_1.Client({ node: `http://${process.env.ELASTICSEARCH_HOST}:9200` });
--- build/models/model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Model;
-(function (Model) {
-    Model["Topic"] = "Topic";
-    Model["User"] = "User";
-    Model["Microblog"] = "Microblog";
-    Model["Job"] = "Job";
-})(Model = exports.Model || (exports.Model = {}));

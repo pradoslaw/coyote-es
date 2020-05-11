f52528845d48aedfe666cd2915d7400df7e7736b
adam <adam@boduch.net>
2020-05-11 19:10:22 +0200

    dodatkowe testy

--- test/builders.test.ts
+++ test/builders.test.ts
@@ -3,17 +3,44 @@ import {Model} from "../src/types/model";
 import esb from 'elastic-builder';
 
 test('build simple query with one model', () => {
-  const builder = new SearchBuilder({models: [Model.Topic]}, {iss: 0, allowed: [1]});
+  const builder = new SearchBuilder({models: [Model.Topic]}, {allowed: [1]});
   const result = builder.build();
 
   expect(result).toBeInstanceOf(esb.RequestBodySearch);
 
   const json = result.toJSON();
+  // @ts-ignore
+  const must = json['query']['function_score']['query']['bool']['must'];
+
+  expect(must[1]['terms']['model']).toEqual(['Topic']);
+  expect(must[0]['bool']['should'][0]['terms']['forums.id']).toEqual([1]);
+  expect(must[0]['bool']['should'][1]['bool']['must_not']['exists']['field']).toEqual('forum.id');
+});
+
+test('build with query', () => {
+  const builder = new SearchBuilder({query: 'test'}, {allowed: [1]});
+  const result = builder.build();
+
+  const json = result.toJSON();
 
   // @ts-ignore
-  expect(json['query']['function_score']['query']['bool']['must'][1]['terms']['model']).toEqual(['Topic']);
+  const should = json['query']['function_score']['query']['bool']['should'];
+
+  expect(should[0]['bool']['must']['simple_query_string']['query']).toEqual('test');
+  expect(should[1]['nested']['query']['bool']['must']['simple_query_string']['query']).toEqual('test');
+});
+
+test('build with query and user', () => {
+  const builder = new SearchBuilder({query: 'test', userId: 1}, {iss: 1, allowed: [1]});
+  const result = builder.build();
+
+  const json = result.toJSON();
+
   // @ts-ignore
-  expect(json['query']['function_score']['query']['bool']['must'][0]['bool']['should'][0]['terms']['forums.id']).toEqual([1]);
-  // @ts-ignore
-  expect(json['query']['function_score']['query']['bool']['must'][0]['bool']['should'][1]['bool']['must_not']['exists']['field']).toEqual('forum.id');
+  const should = json['query']['function_score']['query']['bool']['should'];
+
+  expect(should[0]['bool']['must'][1]['simple_query_string']['query']).toMatch('test');
+  expect(should[0]['bool']['must'][0]['match']['user_id']).toEqual(1);
+  expect(should[1]['nested']['query']['bool']['must'][0]['match']['user_id']).toEqual(1);
+  expect(should[1]['nested']['query']['bool']['must'][1]['simple_query_string']['query']).toMatch('test');
 });

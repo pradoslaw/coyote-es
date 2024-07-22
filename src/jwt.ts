e4b43dce1bdcbc0e4917449147b89f66a57f7598
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-07-22 09:37:21 +0200

    Add required {algorithms:} parameter to "express-jwt"

--- src/jwt.ts
+++ src/jwt.ts
@@ -1,12 +1,12 @@
 import * as express from "express";
-import jwt from "express-jwt";
+import {expressjwt as jwt} from "express-jwt";
 
 export default function jwtHandler(credentialsRequired: boolean) {
   return [
-    jwt({secret: process.env.APP_KEY!, credentialsRequired}),
+    jwt({secret: process.env.APP_KEY!, credentialsRequired, algorithms: ['HS256']}),
     (req: express.Request, res: express.Response, next: Function) => {
       if (req.user) {
-        req.user.iss = parseInt(String(req.user.iss))
+        req.user.iss = parseInt(String(req.user.iss));
       }
 
       next();

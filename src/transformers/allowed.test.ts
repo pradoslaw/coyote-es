e73bec97d19f4990325e7c5ee3eb03e652122fc1
adam <adam@boduch.net>
2024-07-25 16:47:17 +0200

    poprawienie testow

--- src/transformers/allowed.test.ts
+++ src/transformers/allowed.test.ts
@@ -1,6 +1,7 @@
 import allowed from "./allowed";
 import Hit from "../types/hit";
 import { Model } from "../types/model";
+import {JwtPayload} from "jsonwebtoken";
 
 const h = (): Hit => ({
   forum: {
@@ -26,21 +27,21 @@ const h = (): Hit => ({
 
 test('keep allowed result', () => {
   const hit = h();
-  const jwt: Jwt = {iss: 1, allowed: [1]};
+  const jwt: JwtPayload = {iss: '1', allowed: [1]};
 
   expect(allowed(hit, jwt)).toBeTruthy();
 });
 
 test('remove result if user is not authorized', () => {
   const hit = h();
-  const jwt: Jwt = {iss: 1, allowed: []};
+  const jwt: JwtPayload = {iss: '1', allowed: []};
 
   expect(allowed(hit, jwt)).toBeFalsy();
 });
 
 test('remove guarded result if user is authorized to different forum', () => {
   const hit = h();
-  const jwt: Jwt = {iss: 1, allowed: [2]};
+  const jwt: JwtPayload = {iss: '1', allowed: [2]};
 
   expect(allowed(hit, jwt)).toBeFalsy();
 });
@@ -55,7 +56,7 @@ test('remove hidden category', () => {
   const hit = h();
 
   hit.forum = {id: 1, name: '', slug: '', url: '', is_prohibited: false}
-  const jwt: Jwt = {iss: 1, allowed: [2, 3, 4]};
+  const jwt: JwtPayload = {iss: '1', allowed: [2, 3, 4]};
 
   expect(allowed(hit, jwt)).toBeFalsy();
 });
